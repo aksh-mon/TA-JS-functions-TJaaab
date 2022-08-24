d7d376bb628abbfeb3e2dd392e1a01c3a538b90e
Akshay <akshay4444.ak@gmail.com>
2022-08-24 15:54:11 +0530

    Block2 - Assignmenet Level 2 & 3

---
+++ block-BJaabv/code/index.js
@@ -0,0 +1,156 @@
+// Use default parameter where ever you can
+
+/*
+1. 🎖Write a function named calculateDogAge that:
+  * [ ] Takes 1 argument: your puppy's age.
+  * [ ] Calculates your dog's age based on the conversion rate of 1 human year to 7 dog years.
+  * [ ] Return the result dog age in dog year.
+  * [ ] Add an additional argument to the function that takes the conversion rate of human to dog years.
+*/
+
+function calculateDogAge(age) {
+  // Your code goes here 
+  var dogYears = 7*age;
+    return("Your doggie is " + dogYears + " years old in dog years!");
+ }
+calculateDogAge(2);
+/*
+2. 🎖Write a function named calculateMoviesToWatch that:
+  * [ ] takes 2 arguments: age, number of movies you watch every week. (take 4 weeks per month)
+  * [ ] calculates the number of movies for rest of the life (based on a constant max age).
+  * [ ] Return the total number of movie you will watch.
+*/
+const maxage = 100;
+function calculateMoviesToWatch(yourAge,numberOfMovies) {
+  // Your code goes here
+ return yourAge * numberOfMovies - maxage
+}
+-*
+
+/*
+3. 🎖Create a function called celsiusToFahrenheit:
+  * [ ] Accepts celsius temperature as argument.
+  * [ ] Convert it to fahrenheit and return "NN°F is NN°C"
+*/
+
+function celsiusToFahrenheit(celsius) {
+  // Your code goes here
+  var celsiusInF = (celsius*9)/5 + 32;
+  return(celsius + '°C is ' + celsiusInF + '°F');
+}
+celsiusToFahrenheit()
+/*
+4. 🎖Create a function called fahrenheitToCelsius:
+  * [ ] Accepts fahrenheit temperature as argument.
+  * [ ] Convert it to celsius and return "NN°F is NN°C"
+*/
+
+function fahrenheitToCelsius(fahrenheit) {
+  // Your code goes here
+  var fahrenheitInC = ((fahrenheit - 32)*5)/9;
+  return(fahrenheit + '°F is ' + fahrenheitInC + '°C');
+}
+fahrenheitToCelsius()
+
+/*
+5. 🎖 Write a function pow(n, x) that returns x in power n.
+
+  * [ ] The function accepts two parameter x and n.
+  * [ ] Return x times n
+  * [ ] If the value of n is below 0 return "The number below 1 is not allowed"
+*/
+
+const pow = (n, x) => {
+  if (n == 0)
+  return 1
+
+  let result = 1 
+  while (n != 0){
+    n--
+    result *= x
+  }
+  return result
+}
+
+// Test
+pow(3, 2); // 9
+pow(3, 3); // 27
+pow(1, 100); // 1
+pow(-31, 2); // "The number below 1 is not allowed"
+
+/*
+6. 🎖Write a program that accepts two parameter a number n and a string with possible values of `sum` or `product`
+and return sum or product of 1,…,n. If user enters any other value than `sum` or `product` alert `Not a valid Input`.
+*/
+
+function sumOrProductOfN() {
+  // Your code goes here
+}
+
+sumOrProductOfN(4, 'sum'); // 10
+sumOrProductOfN(4, 'product'); // 24
+sumOrProductOfN(4, 'hello'); // alert "Not a valid Input"
+
+/*
+6. 🎖Write a program that accepts a number n using argument and return the sum of the numbers 1 to n
+*/
+
+function sumOfN(N) {
+  // Your code goes here
+  var total = 0;
+  for(var i = 1; i <= N; i++){
+    total += i;
+  }
+  return total;
+
+}
+
+/*
+7. 🎖Modify the previous program such that only multiples of 5 or 7 are considered in the sum, e.g. n = 20 (5,7,10,14,15,20) 71
+*/
+// Your code goes here
+
+function sumOfN(N){
+  for(let j = 1; j < n + 1; j++)
+    {
+        if (j % 5 == 0 || j % 7 == 0)
+            document.write(j + " ");
+    }
+    return N;
+}
+/*
+8. 🎖Write a function `min` that takes two arguments and returns their minimum.
+*/
+
+function min(firstNum, secondNum) {
+  // Your code goes here
+
+	if ( firstNum < secondNum )
+      return firstNum;
+    else
+      return secondNum;}
+
+min(0, 10);
+min(0, -10);
+
+/*
+9. 🎖Write a JavaScript function which accepts an argument and returns the type of the value.
+*/
+
+function typeCheck(value) {
+  // Your code goes here
+  var dtypes = [Function, RegExp, Number, String, Boolean, Object], x, len;
+    
+if (typeof value === "object" || typeof value === "function") 
+    {
+     for (x = 0, len = dtypes.length; x < len; x++) 
+     {
+            if (value instanceof dtypes[x])
+            {
+                return dtypes[x];
+            }
+      }
+    }
+    
+    return typeof value;
+}
--- block-BJaabw/code/index.js
+++ block-BJaabw/code/index.js
@@ -1,3 +1,4 @@
+
 // Use default parameter where ever you can
 
 /* 1. minToSec
@@ -11,10 +12,13 @@ minToSec(2) ➞ 120
 */
 
 // - Write a Function Decleration for above problem
-function minToSec() {
+function minToSec(minutes) {
   // Your code
+return minutes * 60
 }
 // - Execute the function with required parameter
+
+console.log (minToSec(2));
 
 /* 2. 
 Create a function named isInRange which validates whether a number n is exclusively within the bounds of lower and upper.
@@ -26,10 +30,13 @@ isInRange(1, 10, 19); // false
 */
 
 // - Write a Function Decleration for above problem
-function isInRange() {
+function isInRange(min,max,x) {
   // Your code
+  return ((x-min)*(x-max) <= 0);
 }
 // - Execute the function with required parameter
+
+console.log(isInRange(9,18,9));
 
 /* 2. calculateBMI
 
@@ -49,9 +56,21 @@ Obese: BMI is 30 or more
 
 */
 
-function calculateBMI() {
+function calculateBMI(weight, height) {
   // Your code
-}
+  let bmi = weight / (height * height);
+  if(bmi <= 18.5){
+   return "Underweight" 
+  }else if (bmi > 18.5 && bmi < 24.9 ){
+  return "Normal" 
+  }else if (bmi > 25 &&  bmi < 29.9){
+    return "Overweight"
+  } else { bmi < 30}{
+    return "Obeses"
+  }
+  }
+  
+  console.log(calculateBMI(5000,5));
 
 /* 3. appropiateDrinks
 
@@ -64,9 +83,20 @@ Create a function that take the age are return the appropiate drink based on the
 
 */
 
-function appropiateDrinks() {
+function appropiateDrinks(yourAge) {
   // Your code
+  if(yourAge <= 14){
+    return "drink fruit juice"
+  }else if (yourAge <= 18){
+    return "drink soda"
+  }else if ( yourAge <= 21 ){
+    return "Drink flavoured beer"
+  } else if ( yourAge > 21){
+    return "drink throat-piercing vodka"
+  }
 }
+
+console.log(appropiateDrinks(22));
 
 /* 4. Add two numers or string
 
@@ -79,10 +109,18 @@ Twist is when user passes anything other than number, or string value you should
 
 */
 
-function sum() {
+function sum(value1, value2) {
   // Your code
+  if (value1 === value2){
+    return value1 + value2;
+  } else if (value1 == value2){
+    return value1 + value2
+  } else if (value1 != value2){
+    return "Enter Valid values";
+  }
 }
 
+console.log(sum("2","ji"));
 // Function Test
 sum(2, 4); // 4
 sum('Arya', 'Stark'); // "Arya Stark"
